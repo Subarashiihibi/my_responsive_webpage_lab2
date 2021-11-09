9c6e50296c9d9f04ebefb6c79483d72047702e60
Subarashiihibi <80044018+Subarashiihibi@users.noreply.github.com>
2021-11-09 11:26:29 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+
+let icon = document.getElementsByClassName("icon")[0];
+
+icon.addEventListener('click', responsive_control);
+
+function responsive_control() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+}
